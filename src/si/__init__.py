bf0a5da0bd7577afdfce6afe80e6903edde63b5e
AndreiaGomes06 <105715985+AndreiaGomes06@users.noreply.github.com>
2022-10-28 15:24:12 +0100

    Update __init__.py

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -4,3 +4,8 @@ __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
 __maintainer__ = "Andreia Gomes"
 __email__ = "pg45463@alunos.uminho.pt"
+
+
+# -----------------------------------------------------------------------------------
+# This package is heavily inspired and adapted from https://github.com/vmspereira/si
+# -----------------------------------------------------------------------------------
